0b3926e4105961952d48c0e7710c4c1f9dc024b8
Ka-Bar <133657889+Ka-Bar@users.noreply.github.com>
2024-05-16 16:05:26 +0800

    small bug of memio

--- cpu.srcs/sources_1/new/mem_or_io.v
+++ cpu.srcs/sources_1/new/mem_or_io.v
@@ -5,11 +5,11 @@ module mem_or_io (
     input oi_read,
     input oi_write,
     input [13:0]alu_result_addr,
-    output [2:0]led_control,
+    output reg [2:0]led_control,
     // output Result  // data to register
     // output [31:0] addr_output,  // address to memory
     // output reg [31:0] data_write_output,  // data to memory or io
-    output [2:0]switch_ctrl
+    output reg [2:0]switch_control
 );
 
     // assign addr_output = alu_result_addr;
@@ -18,12 +18,12 @@ module mem_or_io (
     // assign switch_ctrl = io_read ? 1'b1 : 1'b0;
 
     always @(*) begin
-        if (oi_read && alu_result_addr[13:2]==SWITCH_MEM) begin
+        if (oi_read && alu_result_addr[13:2]==`SWITCH_MEM) begin
             switch_control = {alu_result_addr[1:0],1'b1};
         end else begin
             switch_control = 3'b000;
         end
-        if (oi_write && alu_result_addr[13:2]==LED_MEM) begin
+        if (oi_write && alu_result_addr[13:2]==`LED_MEM) begin
             led_control = {alu_result_addr[1:0],1'b1};
         end else begin
             led_control = 3'b000;
